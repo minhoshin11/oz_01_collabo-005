28c1ddebdaf927f544497e729f96b493bae3aedf
minhoshin11 <tlsalsgh777@naver.com>
2024-04-22 18:27:56 +0900

    서버 연결 재시도

--- frontend/src/Apis/axios.ts
+++ frontend/src/Apis/axios.ts
@@ -3,8 +3,8 @@
 import axios from "axios";
 const host =
   window.location.hostname === "localhost"
-    ? import.meta.env.VITE_API_URL
-    : "api";
+    ? "http://{http://ec2-13-125-225-5.ap-northeast-2.compute.amazonaws.com/}"
+    : "http://ec2-13-125-225-5.ap-northeast-2.compute.amazonaws.com/";
 
 const instance = axios.create({
   baseURL: host,
